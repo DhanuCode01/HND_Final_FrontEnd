1809011538251e302485f7cc3d6e727ae1a8f4ab
DhanuCode01 <lakshanshanaka327@gmail.com>
2025-03-31 13:45:00 +0530

    Register page And Login page Done👌

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import LoginPage from './pages/login/loginPage'
+import RegisterPage from './pages/register/registerpage'
+
 
 function App() {
  
 
   return (
-    <>
-     
-    </>
+    
+     <BrowserRouter>
+            <Toaster position='top-right'/>
+            <Routes>
+              <Route path='/login' element={<LoginPage/>}/>
+              <Route path="/register" element={<RegisterPage/>} />
+            </Routes>
+     </BrowserRouter>
+   
   )
 }
 
